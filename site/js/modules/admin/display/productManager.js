c7d4a6e46ca547461922828834c58b02c9c5581e
Pål Nicholas Bakken <nicholas@Pals-Air.home>
2022-05-01 00:07:03 +0200

    added more to product management system

--- site/js/modules/admin/display/productManager.js
+++ site/js/modules/admin/display/productManager.js
@@ -1,15 +1,19 @@
 import checkLogin from "../utils/checkLogin.js";
 import getOneProduct from "../../utils/content/products/getOneProduct.js";
 import { unpackProductDetails } from "../../display/components/productDisplay.js";
+import setBackgroundImage from "../../utils/content/setBackgroundImage.js";
 
 const queryString = window.location.search;
 const urlParams = new URLSearchParams(queryString);
 
+let editing;
+
 if(urlParams.get("product_id")) {
     console.log("Has product_id: " + urlParams.get("product_id"));
     displayEditProduct(urlParams.get("product_id"));
+    editing = true;
 } else {
-    attachNewProductListener();
+    editing = false;
 }
 
 async function displayEditProduct(id) {
@@ -21,9 +25,19 @@ async function displayEditProduct(id) {
         console.log(product);
         const title = document.querySelector("#product-title");
         const price = document.querySelector("#product-price");
+        const featured = document.querySelector("#is-featured");
+        const description = document.querySelector("#product-description");
+
+        const productImage = document.querySelector(".product-image");
+        const imageURL = document.querySelector("#product-image-url");
 
         title.value = product.title;
         price.value = product.price;
+        featured.checked = product.featured;
+        description.value = product.description;
+
+        setBackgroundImage(product.image, productImage);
+        imageURL.value = product.image;
 
     }
 }
